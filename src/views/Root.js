6ac9b72863225d75466bfb0015a9c6affce8e68a
Oskar <jankowiak31@gmail.com>
2021-07-10 13:23:22 +0200

    added support for css modules

---
+++ src/views/Root.js
@@ -0,0 +1,18 @@
+import React from "react";
+import "../assets/styles/globals.css";
+
+const Root = () => {
+	return (
+		<div className="app">
+			<h1>Hello Reasdadct</h1>
+			<p>
+				Lorem ipsum dolor sit amet consectetur adipisicing elit. Suscipit, a.
+				Necessitatibus itaque maxime numquam tempora praesentium tempore cum
+				harum ipsum, voluptatum molestiae mollitia, modi quas labore ea
+				molestias sequi atque.
+			</p>
+		</div>
+	);
+};
+
+export default Root;
--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,8 @@
 const path = require("path");
-const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
-const HtmlWebpackRootPlugin = require("html-webpack-root-plugin");
 
 module.exports = {
+	target: "web",
 	entry: path.resolve(__dirname, "./src/index.js"),
 	devtool: "eval-source-map",
 	module: {
@@ -15,7 +14,26 @@ module.exports = {
 			},
 			{
 				test: /\.css$/i,
-				use: [MiniCssExtractPlugin.loader, "css-loader"],
+				use: [
+					"style-loader",
+					{
+						loader: "css-loader",
+						options: {
+							importLoaders: 1,
+							modules: true,
+						},
+					},
+				],
+				include: /\.module\.css$/,
+			},
+			{
+				test: /\.css$/,
+				use: ["style-loader", "css-loader"],
+				exclude: /\.module\.css$/,
+			},
+			{
+				test: /\.svg$/,
+				use: "file-loader",
 			},
 		],
 	},
@@ -28,12 +46,11 @@ module.exports = {
 	},
 	devServer: {
 		port: 5000,
-		publicPath: "/",
-		contentBase: "./public",
-		hotOnly: true,
+		contentBase: path.join(__dirname, "public"),
+		hot: true,
+		open: true,
 	},
 	plugins: [
-		new MiniCssExtractPlugin({ filename: "[name].[contenthash].css" }),
 		new HtmlWebpackPlugin({
 			title: "Hot Module Replacement",
 			templateContent: `
